07132fe00d9ab14de79ceda449791e83a2fce450
rana zaid <itszaidrajpoot@gmail.com>
2024-05-14 02:48:09 +0500

    add code

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-const dropdowns =document.querySelectorAll("dropdown")
+const BASE_URL = "https://v6.exchangerate-api.com/v6/b596e3b857d468b6c57d39fe/latest/USD"
 
+const dropdowns = document.querySelectorAll(".dropdown select")
+
+for(let select of dropdowns) {
+    for(currcode in countryList){
+        const newOption = document.createElement("options");
+        newOption.innerText = currcode ; 
+        newOption.value = currcode ;
+        select.append(newOption)
+    }
+}
